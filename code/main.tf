28d4c9e424f0138dff34c0cf9c1530a922da8aa8
Luke Bibby <luke.bibby@gmail.com>
2022-02-23 08:31:11 +1100

    initial commit

---
+++ code/main.tf
@@ -0,0 +1,9 @@
+/* Resource to create */
+
+resource "aws_vpc" "management_vpc" {
+    cidr_block = var.mgmt_vpc_cidr_block
+
+    tags = {
+        Name = "ManagementVPC"
+    }
+}
---
+++ code/variables.tf
@@ -0,0 +1,5 @@
+variable "mgmt_vpc_cidr_block" {
+    type = string
+    description = "Management VPC CIDR Block"
+    default = "10.1.1.0/24"
+}
