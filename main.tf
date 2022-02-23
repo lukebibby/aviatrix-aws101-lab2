06ae7a73073e57d61c70bdcc8393ad2a55413a35
Luke Bibby <luke.bibby@gmail.com>
2022-02-23 11:52:32 +1100

    refactored vpc creation with for_each

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
 /* Resource to create */
 
-resource "aws_vpc" "management_vpc" {
-  cidr_block = var.vpcs.mgmt_vpc.cidr_block
+resource "aws_vpc" "vpcs" {
+  for_each = var.vpcs
+  
+  cidr_block = each.cidr_block
 
   tags = {
-    Name = var.vpcs.mgmt_vpc.name
-  }
-}
-
-resource "aws_vpc" "services_vpc" {
-  cidr_block = var.vpcs.services_vpc.cidr_block
-
-
-  tags = {
-    Name = var.vpcs.services_vpc.name
+      Name = each.name
   }
 }
