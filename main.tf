9e355d68ee8d353fdfbeec2834f6ef64375d8a3c
Luke Bibby <luke.bibby@gmail.com>
2022-02-24 08:14:56 +1100

    create subnets with for_each

--- main.tf
+++ main.tf
@@ -1,11 +1,23 @@
 /* Resource to create */
 
+/* VPC Creation */
 resource "aws_vpc" "vpcs" {
-  for_each = var.vpcs
-  
-  cidr_block = each.value["cidr_block"]
-
+  for_each   = var.vpcs
+  cidr_block = each.value.cidr_block
   tags = {
-      Name = each.value["name"]
+    Name = each.value.name
   }
 }
+
+/* Subnets */
+resource "aws_subnet" "tgw" {
+  for_each   = var.vpcs
+  vpc_id     = aws_vpc.vpcs[each.key].id
+  cidr_block = each.value.subnets.cidr_block
+}
+
+/* Transit GW */ /*
+resource "aws_ec2_transit_gateway" "tgw" {
+  description = "SydneyTGW"
+}
+*/
--- terraform.auto.tfvars
+++ terraform.auto.tfvars
@@ -2,6 +2,14 @@ vpcs = {
   mgmt_vpc = {
     cidr_block = "10.1.1.0/24"
     name       = "ManagementVPC"
+    subnets = {
+      private_1 = {
+        cidr_block = "10.1.1.0/28"
+      }
+      private_2 = {
+        cidr_block = "10.1.1.16/28"
+      }
+    }
   }
   services_vpc = {
     cidr_block = "10.1.2.0/24"
